425373f0324207dd7c2566f77db14c4cdfb568ac
joaocelson <joaocelson@gmail.com>
2016-01-14 17:00:03 -0200

    Upload Imagem para o servidor.
    Enviado para o servidor imagem formato PNG utilizando o JSON, necessario
    a partir de agora padronizar as classes C# com Android.

--- app/src/main/java/com/jctecnologia/turismoapp/casa/CadastroCasaActivity.java
+++ app/src/main/java/com/jctecnologia/turismoapp/casa/CadastroCasaActivity.java
@@ -15,11 +15,20 @@ import android.widget.ImageView;
 
 import com.jctecnologia.turismoapp.R;
 
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Date;
 
 public class CadastroCasaActivity extends AppCompatActivity {
 
@@ -59,7 +68,9 @@ public class CadastroCasaActivity extends AppCompatActivity {
         final String telefoneCasa2 = txtTelefoneCasa2.getText().toString();
         iv  = (ImageView)findViewById(R.id.ivFotoPerfilCasa);
 
-        Bitmap bm=((BitmapDrawable)iv.getDrawable()).getBitmap();
+        Bitmap bm =((BitmapDrawable)iv.getDrawable()).getBitmap();
+        //bm.setHasAlpha(true);
+
         final String imgString = Base64.encodeToString(getBytesFromBitmap(bm), Base64.NO_WRAP);
 
         class TheTask extends AsyncTask<String, Void, String> {
@@ -69,30 +80,71 @@ public class CadastroCasaActivity extends AppCompatActivity {
             protected String doInBackground(String... arg0) {
                 String text = null;
                 try {
-                    URL url = new URL("http://turismo.somee.com/Estabelecimentos/Cadastro");
+                    URL url = new URL("http://192.168.25.176:85/Estabelecimentos/Create");
                     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                     try {
                         urlConnection.setDoOutput(true);
-                        urlConnection.setChunkedStreamingMode(0);
+                        urlConnection.setDoInput(true);
                         urlConnection.setRequestMethod("POST");
-                        urlConnection.setDoOutput(true);
+                        //urlConnection.setFixedLengthStreamingMode(data.getBytes().length);
+                        urlConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+
+
+
+                        JSONObject jsonObject = new JSONObject();
+
+                        jsonObject.put("EstabelecimentoId", 0);
+                        jsonObject.put("Nome", nomeCasa);
+                        jsonObject.put("NomeFotoPerfil", imgString);
+                        jsonObject.put("DataCadastro",new Date().getDate());
+                        jsonObject.put("PessoaId", 1);
+                        jsonObject.put("Valor", 1);
+                        jsonObject.put("Disponivel", true);
+
+                        String data = jsonObject.toString();
+
+                        OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                        writer.write(data);
+
+                        writer.flush();
+                        writer.close();
+                        out.close();
+                        urlConnection.connect();
+
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                                in, "UTF-8"));
+                        StringBuilder sb = new StringBuilder();
+                        String line = null;
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line);
+                        }
+                        in.close();
+                        String result = sb.toString();
+                        //Log.d("Vicky", "Response from php = " + result);
+                        //Response = new JSONObject(result);
+                        urlConnection.disconnect();
+
+/*
                         String urlParamenter = "nomeCasa=" +
                                 nomeCasa + "&numeroPessoas=" +
                                 numeroPessoas + "&telefoneCasa=" +
                                 telefoneCasa + "&fotoPerfil=" +
                                 imgString + "&idPessoa" +
                                 1;
+*/
 
-                        Log.println(0,"LOG",urlParamenter);
+//                        Log.println(0,"LOG",urlParamenter);
 
-                        OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+                       /* OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
                         out.write(urlParamenter.getBytes());
                         out.flush();
                         out.close();
                         int responseCode = urlConnection.getResponseCode();
                         if (responseCode == 200) {
                             resultado = "true";
-                        }
+                        }*/
                     } catch (Exception e) {
                         e.printStackTrace();
                     } finally {
@@ -110,13 +162,13 @@ public class CadastroCasaActivity extends AppCompatActivity {
             }
         }
 
-        new TheTask().execute("http://turismo.somee.com/Estabelecimentos/Cadastro");
+        new TheTask().execute("http://192.168.25.176:85/Estabelecimentos/Create");
     }
 
     public byte[] getBytesFromBitmap(Bitmap bitmap) {
         try {
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
             return stream.toByteArray();
         } catch (Exception e) {
             e.printStackTrace();
